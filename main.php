d85b37379b7ee65497a412afc99524b2b36a30d7
Mario Tonnarelli <tonnarellilaurent@gmail.com>
2026-09-16 03:45:08 +0200

    Esercizio PHP 01

---
+++ main.php
@@ -0,0 +1,10 @@
+<?php
+$int = 10;
+$float = 1.5;
+$string = "stringa";
+$bool = true;
+const NOME = "Donato";
+var_dump($int, $float,$string,$bool);
+echo NOME;
+// NOME = "Giuseppe"; Parse error: syntax error, unexpected token "="
+// echo NOME;
